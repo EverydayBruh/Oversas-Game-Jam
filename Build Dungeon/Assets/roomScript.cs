7232ef409a143ff909d5c8995bc00335d0fb7933
EverydayBruh <roliki2222222@gmail.com>
2023-07-08 00:05:45 +0300

    Project Created

---
+++ Build Dungeon/Assets/roomScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+
+    public Room[] neighbors = new Room[4];
+    
+    public void SetNeighbors(Room top, Room right, Room bottom, Room left)
+    {
+        neighbors[0] = top;
+        neighbors[1] = right;
+        neighbors[2] = bottom;
+        neighbors[3] = left;
+    }
+
+    public void AddRoom(Direction direction, Room newRoom)
+    {
+        int index = (int)direction;
+
+        if (index >= 0 && index < neighbors.Length)
+        {
+            neighbors[index] = newRoom;
+            newRoom.neighbors[(index + 2) % 4] = this;
+        }
+    }
+}
+
+// ѕеречисление дл€ представлени€ направлений
+public enum Direction
+{
+    Top = 0,
+    Right = 1,
+    Bottom = 2,
+    Left = 3
+}
